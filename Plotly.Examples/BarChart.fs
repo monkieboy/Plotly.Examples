ff6a3c75e880e4242a259e4fa6e4d24ca3135047
info@millbank <in@magsolutionslimited.co.uk>
2015-05-12 22:02:25 +0100

    First stuff (mostly rubbish)

---
+++ Plotly.Examples/BarChart.fs
@@ -0,0 +1,37 @@
+﻿namespace Plotly.Examples.BarChart
+
+open XPlot.GoogleCharts
+open Plotly.Examples.PremierLeague.Data
+open System
+
+module BarChart =
+    let data = prem2013
+    let convertScore (s:String) =
+        let split = s.Split([|"-"|], StringSplitOptions.None)
+        Convert.ToInt32 split.[0], Convert.ToInt32 split.[1]
+
+    
+    let rows = data.Rows
+    
+    
+    let convertedRows = 
+        rows 
+        |> Seq.map (fun r -> (r.Date,r.``Team 1``,r.``Team 2``, r.FT |> convertScore, r.HT |> convertScore))
+    
+    let calcGoals s =
+        let a, b = s
+        a + b
+    
+    let goalsScored = 
+        let goals = 
+            convertedRows 
+            |> Seq.map (fun (_, _, _, f, _) -> f |> calcGoals )
+        goals |> Seq.sum
+        
+
+    printfn "%i goals scored in the season should equal 1063." goalsScored
+
+
+
+
+
---
+++ Plotly.Examples/Prem2013.fs
@@ -0,0 +1,28 @@
+﻿namespace Plotly.Examples.PremierLeague
+
+    open FSharp.Data
+    open System
+    open System.IO
+    open System.Net
+
+    type PremData = FSharp.Data.CsvProvider<"Date,Team 1,Team 2,FT,HT
+    2013-08-17,Arsenal,Aston Villa,1-3,1-1
+    2013-08-17,Liverpool,Stoke,1-0,1-0">
+
+    module Data =
+
+
+        let uri = new Uri("https://raw.githubusercontent.com/footballcsv/en-england/master/2010s/2013-14/1-premierleague.csv")
+
+        let req = WebRequest.Create(uri);
+        req.ContentType <- "text\csv"
+        let res = req.GetResponse()
+  
+        let rdr = new StreamReader(res.GetResponseStream())
+        let csv = rdr.ReadToEnd()
+        rdr.Close()
+
+        let prem2013 = PremData.Load "https://raw.githubusercontent.com/footballcsv/en-england/master/2010s/2013-14/1-premierleague.csv"
+
+
+
